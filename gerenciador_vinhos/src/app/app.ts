c1f93fb53e2c4f62ac989df8338c957d2a9a10fe
Bruno <brunoipb1@gmail.com>
2026-04-28 17:10:20 +0000

    feat: modelo vinho

--- gerenciador_vinhos/src/app/app.ts
+++ gerenciador_vinhos/src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+
+interface Vinho {
+  id: number;
+  nome: string;
+  preco: number;
+  tipo: string;
+  disponivel: boolean;
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('gerenciador_vinhos');
+
+  // Persistencia
+  vinhos: Vinho[] = [
+    { id: 1, nome: 'Bons Ventos', preco: 52.99, tipo: 'Seco', disponivel: true },
+  ];
+
+  // Campos do formulario
+  nextId: number = 2;
+  nome: string = '';
+  preco: number | null = null;
+  tipos: string[] = ['Suave', 'Seco', 'Branco', 'Tinto'];
+  disponivel: boolean = false;
+
+  tipoSelected: string = 'Suave';
+  idEmEdicao: number | null = null;
+
+
 }
