ee3cf6cb1c8085f73941ba41ca2266fad3360dc2
Bruno <brunoipb1@gmail.com>
2026-04-28 17:13:53 +0000

    feat: implenta template do crud

--- gerenciador_vinhos/src/app/app.ts
+++ gerenciador_vinhos/src/app/app.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table'
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { FieldsetModule } from 'primeng/fieldset';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { DecimalPipe } from '@angular/common';
 
 interface Vinho {
   id: number;
@@ -13,10 +27,24 @@ interface Vinho {
   selector: 'app-root',
   standalone: true,
   imports: [
-    
+    ButtonModule,
+    TableModule,
+    FormsModule,
+    InputNumberModule,
+    InputGroupModule,
+    InputTextModule,
+    SelectModule,
+    InputGroupAddonModule,
+    ToggleButtonModule,
+    FieldsetModule,
+    ConfirmDialogModule,
+    DialogModule,
+    ToastModule,
+    DecimalPipe
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  providers: [ConfirmationService, MessageService]
 })
 export class App {
 
@@ -35,5 +63,9 @@ export class App {
   tipoSelected: string = 'Suave';
   idEmEdicao: number | null = null;
 
+  // Dependencias do dialog
+  confirmationService = inject(ConfirmationService);
+  messageService = inject(MessageService);
+  visible: boolean = false;
 
 }
